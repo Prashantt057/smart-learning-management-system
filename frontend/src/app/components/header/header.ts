a47e78b341329b8d818ff870dc5bbbac8325e390
VishalVibe <136216047+VishalVibe@users.noreply.github.com>
2026-06-11 21:40:34 +0530

    added styling in enrollment page

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  get greeting(): string {
+    const hours = new Date().getHours();
+    if (hours < 12) {
+      return 'Good Morning';
+    } else if (hours < 16) {
+      return 'Good Afternoon';
+    } else {
+      return 'Good Evening';
+    }
+  }
+}
